4079cddcab77df1090fbc2b47371efdd468f80ee
krisikostadinov <krisi.199898@gmail.com>
2023-05-16 01:57:01 +0300

    add auth guard

--- server/src/config/middlewares.js
+++ server/src/config/middlewares.js
@@ -1,3 +1,5 @@
+const { verifyToken, decryptToken } = require("../utils");
+
 const errorHandler = (err, req, res, next) => {
   if (err) {
     res.status(500).send({
@@ -8,6 +10,31 @@ const errorHandler = (err, req, res, next) => {
   next();
 };
 
+const isAuth = (req, res, next) => {
+  const unsplittedToken = req.headers.authorization;
+
+  if (!unsplittedToken) {
+    res.status(400).send({ message: "The token can not be null." });
+    return;
+  }
+
+  const token = unsplittedToken.split(" ")[1];
+
+  const decryptedToken = decryptToken(token);
+
+  const decoded = verifyToken(decryptedToken);
+
+  if (!decoded) {
+    res.status(400).send({ message: "Invalid token." });
+    return;
+  }
+
+  req.user = decoded;
+
+  next();
+}
+
 module.exports = {
   errorHandler,
+  isAuth,
 };
--- server/src/routers/questions.js
+++ server/src/routers/questions.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const questions = require("../controllers/questions");
+const { isAuth } = require("../config/middlewares");
 
 router.get("/random", questions.get.random);
 router.get("/:id", questions.get.byId);
 
-router.post("/", questions.post.save);
+router.post("/", isAuth, questions.post.save);
 
-router.delete("/:id", questions.del.byId);
+router.delete("/:id", isAuth, questions.del.byId);
 
 module.exports = router;
