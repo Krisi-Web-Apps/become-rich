f6bc01998316286f1aa7fb1d88d1252a795aae67
krisikostadinov <krisi.199898@gmail.com>
2023-05-17 01:48:03 +0300

    update the questions router

--- server/src/routers/questions.js
+++ server/src/routers/questions.js
@@ -3,9 +3,9 @@ const questions = require("../controllers/questions");
 const { isAuth, isAdmin } = require("../config/middlewares");
 
 router.get("/random", questions.get.random);
+router.get("/all", isAuth, questions.get.all);
+router.get("/search", isAuth, questions.get.search);
 router.get("/:id", questions.get.byId);
-router.get("/admin/all", questions.get.all);
-router.get("/admin/search", questions.get.search);
 
 router.post("/admin", isAuth, isAdmin, questions.post.save);
 
